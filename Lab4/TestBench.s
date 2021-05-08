dfa2b0528277202bc6e55873311d77641f26e00e
adornelas <andredornelas23@gmail.com>
2021-05-08 17:04:50 -0300

    Problemas de temporização

--- Lab4/TestBench.s
+++ Lab4/TestBench.s
@@ -14,18 +14,18 @@ INICIO:
 	lui a0, 7		# a0 = 0x00007000
 	
 	
-	addi a0, zero, 0
-	addi a1, zero, 1
-	and a2, a0, a1		# a2 = 0x00000000
-	or a2, a0, a1		# a2 = 0x00000001
-	xor a2, a0, a1		# a2 = 0x00000001
+	addi a0, zero, 1	# a0 = 0x00000001
+	addi a1, zero, 3	# a1 = 0x00000003
+	and a2, a0, a1		# a2 = 0x00000001
+	or a2, a0, a1		# a2 = 0x00000003
+	xor a2, a0, a1		# a2 = 0x00000002
 	slt a2, a0, a1		# a2 = 0x00000001
 	
 	jal a3, PULO		# pula pra PULO
 	li a0, 0xEEEEEEEE 	# não deve rodar
 	li a0, 0xEEEEEEEE	# não deve rodar
 
-PULO:	add a1, a0, zero	# a1 = 0x00007000
+PULO:	add a1, a0, zero	# a1 = 0x00000001
 	beq a0, a1, PULO2	# pula pra PULO2
 	li a0, 0xEEEEEEEE	# não deve rodar, a0=0xEEEEEEEE
 	li a0, 0xEEEEEEEE	# não deve rodar, a0=0xEEEEEEEE
@@ -33,7 +33,7 @@ PULO:	add a1, a0, zero	# a1 = 0x00007000
 	li a0, 0xCCCCCCCC	# deve rodar ao final, a0=0xCCCCCCCC
 	jal a3, FIM
 
-PULO2:	li a1, 0x00000000	# a1 = 4
+PULO2:	li a1, 0x00000000	# a1 = 0x00000000
 	jalr a0, a1, 100	# a0 = pc, vai pra ultima instrução
 	li a0, 0xEEEEEEEE	# não deve rodar, a0=0xEEEEEEEE
 	li a0, 0xEEEEEEEE	# não deve rodar, a0=0xEEEEEEEE
